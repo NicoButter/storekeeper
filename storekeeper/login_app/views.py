30d227c12922cf0ed77b9fad0bbd9cde6313d395
Nicolás Butterfield <nicobutter@gmail.com>
2024-06-22 16:21:53 -0300

    mejoras login_app

--- storekeeper/login_app/views.py
+++ storekeeper/login_app/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render, redirect
+from .forms import LoginForm
 from django.contrib.auth import authenticate, login
 
 def login_view(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')  # Redirigir a la página de inicio
-        else:
-            return render(request, 'login.html', {'error': 'Credenciales incorrectas'})
-    return render(request, 'login.html')
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # Redirigir a la página de inicio
+            else:
+                error = 'Credenciales incorrectas'
+                return render(request, 'login.html', {'form': form, 'error': error})
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
