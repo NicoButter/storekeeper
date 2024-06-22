1b1f28af62d366682a6c6e3a2e58c54b47b37a7d
Nicolás Butterfield <nicobutter@gmail.com>
2024-06-22 17:32:29 -0300

    nuevo commit con mejoras

--- storekeeper/login_app/urls.py
+++ storekeeper/login_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from.templates.login_app import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('login/', views.login_view, name='login'),
 ]
--- storekeeper/storekeeper/settings.py
+++ storekeeper/storekeeper/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'login_app'
+    'login_app',
+    'core_app'
 ]
 
 MIDDLEWARE = [
@@ -80,8 +81,12 @@ WSGI_APPLICATION = 'storekeeper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'storekeeper_db',
+        'USER': 'postgres',  # reemplaza con tu usuario de PostgreSQL
+        'PASSWORD': 'postgres010203',  # reemplaza con tu contraseña de PostgreSQL
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
